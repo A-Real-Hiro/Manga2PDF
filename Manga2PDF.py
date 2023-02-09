5c8da36a985dff9e54d298481d7d295c31c4dff0
A Real Hiro <edgar.becerril001@gmail.com>
2023-02-08 19:35:24 -0600

    Added logic for finding cbz files in a given directory and extracting them to folders named after the initial cbz

--- Manga2PDF.py
+++ Manga2PDF.py
@@ -6,6 +6,8 @@ README contains more information regarding this project.
 """
 import tkinter as tk
 import tkinter.messagebox as msgbox
+import os, zipfile
+import PIL
 
 class App(tk.Frame):
     def __init__(self, master):
@@ -29,16 +31,60 @@ class App(tk.Frame):
                              self.textEntryContent,set(self.entryBox["textvariable"]))
 
         # CONVERT BUTTON
-
         self.btnConvert = tk.Button(master, text = "Convert", command = self.convert)
         self.btnConvert.pack()
 
-    def convert(self):
-        cbz_path = self.textEntryContent
+    def toZip(self, inFile, pathForPDF):
+        zip_ref = zipfile.ZipFile(pathForPDF + f"\\{inFile}", 'r')
+        new_dir = pathForPDF + f"\\temp\\{str(inFile[:-4]).rstrip()}\\"
+        zip_ref.extractall(path=new_dir)
+        zip_ref.close()
+        newPDF = inFile.replace(inFile[:-4],".pdf")
+        self.zipToPDF(newPDF,new_dir)
+        # changes file extensions of new files
+        #newFile = inFile.replace(inFile[-4:],'.pdf')
 
-        if (cbz_path is None or "Enter directory here for conversion"):
+
+    def zipToPDF(self, newFileName, newPath):
+        print(f"New name: {newFileName} New Path: {newPath}")
+        chapters = os.listdir(newPath)
+
+        for folder in chapters:
+            for file in os.listdir(newPath + f"\\{folder}"):
+
+
+    def toPDF(self, files: list, pathForPDF):
+        for file in files:
+            self.toZip(file, pathForPDF)
+        
+
+
+    def convert(self):
+        cbz_path = self.textEntryContent.get()
+        cbz_path.replace("/", "\\")
+        print(f"cbz_path: {cbz_path}")
+        print(os.path.exists(cbz_path))
+        # tests if the path is valid
+        if os.path.exists(cbz_path):
+            msgbox.showinfo(title = "Success!", message = "Valid path selected! Verifying presence of cbz files...")
+            fToConvert = 0
+
+            for fname in os.listdir(cbz_path):
+                if '.cbz' in fname: fToConvert += 1
+            if fToConvert == 0:
+                msgbox.showerror(title = "ERROR", message = f"No CBZ files to convert in {cbz_path}")
+                return
+            
+            msgbox.showinfo(title = "Files Found!", message = f"{fToConvert} CBZ files found in {cbz_path}")
+            # switches over to the file path where all the cbz files are located
+            self.toPDF({file for file in os.listdir(cbz_path) if '.cbz' in file}, cbz_path)
+
+        else:
+            # case where valid path was not entered        
             msgbox.showerror(title = "ERROR", message = "Please enter a valid file path.")
-        print(f"The button was pressed with cbz_path as {cbz_path}")
+        
+        return
+        
 
 # configures the window
 window = tk.Tk()
