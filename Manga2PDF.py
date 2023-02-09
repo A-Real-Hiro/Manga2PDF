36184ac4548395b8e5484dd86058d726ba283957
A Real Hiro <edgar.becerril001@gmail.com>
2023-02-08 20:47:36 -0600

    made the program able to convert .cbz files into pdfs

--- Manga2PDF.py
+++ Manga2PDF.py
@@ -7,12 +7,13 @@ README contains more information regarding this project.
 import tkinter as tk
 import tkinter.messagebox as msgbox
 import os, zipfile
-import PIL
+from PIL import Image
 
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.pack()
+        self.cbz_path = ""
 
         self.entryBox = tk.Entry()
         self.label = tk.Label(text="Please enter the file path for where your .cbz files:")
@@ -36,21 +37,31 @@ class App(tk.Frame):
 
     def toZip(self, inFile, pathForPDF):
         zip_ref = zipfile.ZipFile(pathForPDF + f"\\{inFile}", 'r')
-        new_dir = pathForPDF + f"\\temp\\{str(inFile[:-4]).rstrip()}\\"
+        new_dir = pathForPDF + f"\\temp\\" + inFile.replace('.cbz','').rstrip()
         zip_ref.extractall(path=new_dir)
         zip_ref.close()
-        newPDF = inFile.replace(inFile[:-4],".pdf")
+        newPDF = pathForPDF + "\\" + inFile.replace('.cbz','.pdf')
         self.zipToPDF(newPDF,new_dir)
-        # changes file extensions of new files
-        #newFile = inFile.replace(inFile[-4:],'.pdf')
 
 
     def zipToPDF(self, newFileName, newPath):
         print(f"New name: {newFileName} New Path: {newPath}")
-        chapters = os.listdir(newPath)
-
+        
+        chapter_folder = os.listdir(newPath)[0]
+        chapters = os.listdir(newPath + f"\\{chapter_folder}")
+        img_paths = []
         for folder in chapters:
-            for file in os.listdir(newPath + f"\\{folder}"):
+            imgFileNames = [file for file in os.listdir(newPath + f"\\{chapter_folder}\\{folder}") if '.jpg' or '.png' in file]
+            imgPath = newPath + f"\\{chapter_folder}\\{folder}"
+            for file in imgFileNames:
+                img_paths.append(imgPath + f"\\{file}")
+        # sorts the images into order
+        img_paths.sort()
+        # opens the images in a list
+        images = []
+        images = [Image.open(img_path) for img_path in img_paths]
+        images[0].save(newFileName, "PDF", resolution=100.0, save_all=True, append_images=images[1:])
+
 
 
     def toPDF(self, files: list, pathForPDF):
@@ -60,24 +71,25 @@ class App(tk.Frame):
 
 
     def convert(self):
-        cbz_path = self.textEntryContent.get()
-        cbz_path.replace("/", "\\")
-        print(f"cbz_path: {cbz_path}")
-        print(os.path.exists(cbz_path))
+        self.cbz_path = self.textEntryContent.get()
+        self.cbz_path.replace("/", "\\")
+        print(f"cbz_path: {self.cbz_path}")
+        print(os.path.exists(self.cbz_path))
         # tests if the path is valid
-        if os.path.exists(cbz_path):
+        if os.path.exists(self.cbz_path):
             msgbox.showinfo(title = "Success!", message = "Valid path selected! Verifying presence of cbz files...")
             fToConvert = 0
 
-            for fname in os.listdir(cbz_path):
+            for fname in os.listdir(self.cbz_path):
                 if '.cbz' in fname: fToConvert += 1
             if fToConvert == 0:
-                msgbox.showerror(title = "ERROR", message = f"No CBZ files to convert in {cbz_path}")
+                msgbox.showerror(title = "ERROR", message = f"No CBZ files to convert in {self.cbz_path}")
                 return
             
-            msgbox.showinfo(title = "Files Found!", message = f"{fToConvert} CBZ files found in {cbz_path}")
+            msgbox.showinfo(title = "Files Found!", message = f"{fToConvert} CBZ files found in {self.cbz_path}")
             # switches over to the file path where all the cbz files are located
-            self.toPDF({file for file in os.listdir(cbz_path) if '.cbz' in file}, cbz_path)
+            self.toPDF({file for file in os.listdir(self.cbz_path) if '.cbz' in file}, self.cbz_path)
+            msgbox.showinfo(title="Success!", message = "All .cbz files in path were converted to PDFs! Happy reading!")
 
         else:
             # case where valid path was not entered        
